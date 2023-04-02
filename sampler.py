d5af00c8c12a6bff678cf5b798ee469ae3cd768f
kyrlian <kyrlian@gmail.com>
2023-04-02 20:50:08 +0200

    pyaudio

--- sampler.py
+++ sampler.py
@@ -1,67 +1,55 @@
 
-VALMAX = 1.0
-SAMPLERATE = 44100 #Only Capitalized variable are visible outside the package - I used SAMPLERATE for readability
-BUFFERSECONDS = 1
+
+
+import math
+import time
+import sys
+import pyaudio # pip install pyaudio
+
+# https://stackoverflow.com/questions/974071/python-library-for-playing-fixed-frequency-sound
+# https://pypi.org/project/PyAudio/
+# https://realpython.com/playing-and-recording-sound-python/#comparison-of-audio-libraries
 
 class TrackStreamer:
     #Beep Streamer struct
-    def __init__(self,tracksignal,):
-        self.trackpointer   = Signal()
-        self.samplePosition = int()
-        #nbsamples      int //max number of sample based on track's end time and SAMPLERATE
-    
-    def Stream(str, samples):
-        #beep.Streamer interface requires Stream and Err
-        tstart = time.Now()
-        for i in samples :
-            time = float(str.samplePosition) / float(SAMPLERATE)
-            val = str.trackpointer.getval(time)
-            if val > valmax:
-                valmax = val #store and display the max value seen
-                print("WARNING - sampler:valmax=%v\n" % val)
-            samples[i][0] = val
-            samples[i][1] = val
-            str.samplePosition += 1
-        if False: #activate to show computing stats
-            elapsed = time.Since(tstart)
-            microseconds = 1000.0 * 1000.0 * len(samples) / SAMPLERATE
-            duration = time.Duration(microseconds) * time.Microsecond
-            print("    sampler.Stream:Computed %v samples for %v in %v\n" % len(samples), duration, elapsed)
-        return len(samples), True
+    def __init__(self,soundsignal):
+        self.vamlax = 1.0
+        samplerate = 44100
+        channels=2
+        self.bufferseconds = 1
+        self.soundsignal  = soundsignal
+        self.sampleposition = 0
+        amplitude = .1 
+        frequency = 440
 
-    def Err(str):
-        return nil
+        def callback(data, frame_count, time_info, status):
+            # If len(data) is less than requested frame_count, PyAudio automatically
+            # assumes the stream is finished, and the stream stops.
+            if status:
+                print(status, file=sys.stderr)
+            global start_idx
+            t = (start_idx + np.arange(frame_count)) / samplerate
+            t = t.reshape(-1, 1)
+            data[:] = amplitude * math.sin(2 * math.pi * frequency * t)
+            start_idx += frame_count
+            return (data, pyaudio.paContinue)
 
+        # Instantiate PyAudio and initialize PortAudio system resources (2)
+        p = pyaudio.PyAudio()
 
+        # Open stream using callback (3)
+        stream = p.open(format=p.get_format_from_width(1),
+                        channels=self.channels,
+                        rate=self.samplerate,
+                        output=True,
+                        stream_callback=callback)
 
-def trackStreamer(signal):
-    #Infinite streamer
-    return TrackStreamer(signal, 0)
+        # Wait for stream to finish (4)
+        while stream.is_active():
+            time.sleep(0.1)
 
-def timedStreamer(signal, nbseconds):
-    #Finite streamer based on track duration
-    trackstr = trackStreamer(signal)
-    return beep.Take(SAMPLERATE*nbseconds, trackstr)
+        # Close the stream (5)
+        stream.close()
 
-def saveToWav(signal, filename, nbseconds):
-    print("Saving to %v\n" % filename+".wav")
-    file, fileerr = os.CreateTemp("out", filename+"_*.wav")
-    #trackstr := trackStreamer(signal)
-    #timedStreamer := beep.Take(trackstr.nbsamples, &trackstr)
-    timedStreamer = timedStreamer(signal, nbseconds)
-    format = beep.Format(SampleRate= beep.SampleRate(SAMPLERATE), NumChannels= 2, Precision= 2)
-    writeerr = wav.Encode(file, timedStreamer, format)
-
-#Run
-def runSampler(signal, nbseconds):
-    print("Preparing speaker\n")
-    sampleRate = beep.SampleRate(SAMPLERATE)
-    buffSize = bufferseconds * sampleRate.N(time.Second)
-    print("    sampler.initSpeaker:buffSize=%v\n" % buffSize)
-    speaker.Init(sampleRate, buffSize)
-    print("Preparing streamer\n")
-    #var trackstr = trackStreamer(signal)
-    timedStreamer = timedStreamer(signal, nbseconds)
-    print("Playing speaker\n")
-    speaker.Play(timedStreamer)
-    select {}
+        # Release PortAudio system resources (6)
+        p.terminate()
